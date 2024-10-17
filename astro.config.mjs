9d9ae74ae1089ed9a5661566d06011a9b768cf00
Mihirp6 <mihir.patel@innovationcharter.org>
2024-10-17 13:26:21 +0000

    MP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
 export default defineConfig({
   vite: {
     build: {
       minify: false,
+     
     },
   },
+  integrations: [react()],
 });
